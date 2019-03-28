e13e9f79b607073f0fcf3be9aee3847b3310988e
Administrator <Administrator@PC-20170930RVDD>
2019-03-28 18:48:07 +0800

    初次提交springboot的模板

---
+++ src/main/java/org/fkjava/weixin1/Weixin1Application.java
@@ -0,0 +1,13 @@
+package org.fkjava.weixin1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weixin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Weixin1Application.class, args);
+	}
+
+}
